7d175c9274a5b0e96a64d061de80715ab02feb34
fujishige-bee <rock8bee@gmail.com>
2025-05-22 18:03:35 +0900

    [add]CreateUserUsecase追加（途中）

---
+++ src/User/application/create-user.usecase.ts
@@ -0,0 +1,9 @@
+import { CreateUserInput } from './create-user.input'
+
+export class CreateUserUsecase {
+  constructor() {}
+
+  async handle(input: CreateUserInput) {
+    // NOTE: 5/22 ここまで
+  }
+}
